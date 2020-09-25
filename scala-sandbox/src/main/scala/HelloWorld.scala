f140f0feb64e1d5e990f56d21b2b148f4845bdd1
kyu08 <kyu08@users.noreply.github.com>
2020-09-25 21:54:57 +0900

    2 WIP 制御今分

--- scala-sandbox/src/main/scala/HelloWorld.scala
+++ scala-sandbox/src/main/scala/HelloWorld.scala
@@ -1,26 +1,17 @@
-import scala.concurrent.{Await, Future, Promise}
-
 object HelloWorld {
-  println("🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺")
   def main(args: Array[String]): Unit = {
-    val promiseGetInt: Promise[Int] = Promise[Int]
-    val futureByPromise: Future[Int] = promiseGetInt.future // PromiseからFutureを作ることが出来る
+    println("🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺")
+    val taro = "john"
 
-    // Promiseが解決されたときに実行される処理をFutureを使って書くことが出来る
-    val mappedFuture = futureByPromise.map { i =>
-      println(s"Success! i: ${i}")
+    val hogehoge = taro match {
+      case "Taro" | "john" => "tarooo"
+      case "Jiro" => "Jirooo"
+      case _ => "noooooo"
     }
 
-    // 別スレッドで何か重い処理をして、終わったらPromiseに値を渡す
-    Future {
-      Thread.sleep(300)
-      promiseGetInt.success(1)
-    }
+    println(hogehoge)
 
-    Await.ready(mappedFuture, 5000.millisecond)
-
+    println("🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺")
   }
-
-  println("🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺")
 }
 
